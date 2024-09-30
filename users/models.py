4748c98ef31a3896b4167d249a0faf7cf30e653e
Ayta Dzhivanov <kurt.alive.1994@gmail.com>
2024-09-30 16:45:22 +0300

    more user fixes

--- users/models.py
+++ users/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.hashers import make_password
 from django.contrib.auth.models import UserManager, AbstractUser
-from django.db import models
 
 
 class CustomUserManager(UserManager):
